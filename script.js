f865f0567eb4d662a8e70abb5324ee60bd028546
Luiz Felipe Melo <160558700+Felipemelo48@users.noreply.github.com>
2025-01-30 11:45:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contato = document.querySelector("#contato p a");
+    contato.addEventListener("click", function () {
+        alert("Me envie um email para conversarmos!");
+    });
+});
